47b898138bdbb16aa3f40eec319976f83872a774
Edanflame <237397488@qq.com>
2021-09-07 11:19:41 +0800

    添加turnover和localtime字段

--- vnpy_arctic/arctic_database.py
+++ vnpy_arctic/arctic_database.py
@@ -50,6 +50,7 @@ class ArcticMongoDatabase(BaseDatabase):
             test_dict["date"].append(convert_tz(bar.datetime))
             test_dict["interval"].append(bar.interval.value)
             test_dict["volume"].append(bar.volume)
+            test_dict["turnover"].append(bar.turnover)
             test_dict["open_interest"].append(bar.open_interest)
             test_dict["open_price"].append(bar.open_price)
             test_dict["high_price"].append(bar.high_price)
@@ -95,6 +96,7 @@ class ArcticMongoDatabase(BaseDatabase):
 
             test_dict["name"].append(tick.name)
             test_dict["volume"].append(tick.volume)
+            test_dict["turnover"].append(tick.turnover)
             test_dict["open_interest"].append(tick.open_interest)
             test_dict["last_price"].append(tick.last_price)
             test_dict["last_volume"].append(tick.last_volume)
@@ -129,11 +131,13 @@ class ArcticMongoDatabase(BaseDatabase):
             test_dict["ask_volume_3"].append(tick.ask_volume_3)
             test_dict["ask_volume_4"].append(tick.ask_volume_4)
             test_dict["ask_volume_5"].append(tick.ask_volume_5)
+
+            test_dict["localtime"].append(convert_tz(tick.localtime))
         data_frame = pd.DataFrame(test_dict)
 
         # 使用update操作将数据更新到数据库中
         bar_symbol = symbol + "-" + exchange + "-" + "tick"
-        self.library.update(bar_symbol, data_frame, upsert=True, chunk_size="S")
+        self.library.update(bar_symbol, data_frame, upsert=True)
 
     def load_bar_data(
         self,
@@ -148,12 +152,13 @@ class ArcticMongoDatabase(BaseDatabase):
         if bar_symbol in self.library.list_symbols():
             bars: List[BarData] = []
             df = self.library.read(bar_symbol, chunk_range=pd.date_range(start, end))
-            for date, symbol, exchange_d, interval, volume, open_interest, open_price, high_price, low_price, close_price\
-                in zip(df["date"], df["symbol"], df["exchange"], df["interval"], df["volume"], df["open_interest"],
-                       df["open_price"], df["high_price"], df["low_price"], df["close_price"]):
+            for date, symbol, exchange_d, interval, volume, turnover, open_interest, open_price, high_price, low_price, close_price\
+                in zip(df["date"], df["symbol"], df["exchange"], df["interval"], df["volume"], df["turnover"],
+                       df["open_interest"], df["open_price"], df["high_price"], df["low_price"], df["close_price"]):
                 bar_datetime = datetime.fromtimestamp(date.timestamp(), DB_TZ)
                 bar = BarData("DB", symbol, exchange, bar_datetime)
                 bar.volume = volume
+                bar.turnover = turnover
                 bar.interval = Interval(interval)
                 bar.open_interest = open_interest
                 bar.open_price = open_price
@@ -178,25 +183,27 @@ class ArcticMongoDatabase(BaseDatabase):
             tick_symbol = symbol + "-" + exchange.value + "-" + "tick"
             df = self.library.read(tick_symbol, chunk_range=pd.date_range(start, end))
             ticks: List[TickData] = []
-            for symbol, exchange_t, date, name, volume,\
+            for symbol, exchange_t, date, name, volume, turnover,\
                 open_interest, last_price, last_volume, limit_up, limit_down,\
                 open_price, high_price, low_price, pre_close,\
                 bid_price_1, bid_price_2, bid_price_3, bid_price_4, bid_price_5,\
                 ask_price_1, ask_price_2, ask_price_3, ask_price_4, ask_price_5,\
                 bid_volume_1, bid_volume_2, bid_volume_3, bid_volume_4, bid_volume_5,\
-                ask_volume_1, ask_volume_2, ask_volume_3, ask_volume_4, ask_volume_5\
-                in zip(df["symbol"], df["exchange"], df["date"], df["name"], df["volume"],
+                ask_volume_1, ask_volume_2, ask_volume_3, ask_volume_4, ask_volume_5, localtime\
+                in zip(df["symbol"], df["exchange"], df["date"], df["name"], df["volume"], df["turnover"],
                        df["last_price"], df["last_volume"], df["limit_up"], df["limit_down"],
                        df["open_price"], df["high_price"], df["low_price"], df["pre_close"],
                        df["bid_price_1"], df["bid_price_2"], df["bid_price_3"], df["bid_price_4"], df["bid_price_5"],
                        df["ask_price_1"], df["ask_price_2"], df["ask_price_3"], df["ask_price_4"], df["ask_price_5"],
                        df["bid_volume_1"], df["bid_volume_2"], df["bid_volume_3"], df["bid_volume_4"], df["bid_volume_5"],
-                       df["ask_volume_1"], df["ask_volume_2"], df["ask_volume_3"], df["ask_volume_4"], df["ask_volume_5"]):
+                       df["ask_volume_1"], df["ask_volume_2"], df["ask_volume_3"], df["ask_volume_4"], df["ask_volume_5"],
+                       df["localtime"]):
                 tz_time = datetime.fromtimestamp(date.timestamp(), DB_TZ)
                 tick = TickData("DB", symbol, exchange, tz_time)
 
                 tick.name = name
                 tick.volume = volume
+                tick.turnover = turnover
                 tick.open_interest = open_interest
                 tick.last_price = last_price
                 tick.last_volume = last_volume
@@ -231,6 +238,9 @@ class ArcticMongoDatabase(BaseDatabase):
                 tick.ask_volume_3 = ask_volume_3
                 tick.ask_volume_4 = ask_volume_4
                 tick.ask_volume_5 = ask_volume_5
+
+                tick.localtime = localtime
+                
                 ticks.append(tick)
             return ticks
         else:
@@ -249,7 +259,7 @@ class ArcticMongoDatabase(BaseDatabase):
             info = self.library.get_info(bar_symbol)
             self.library.delete(bar_symbol)
         else:
-            return "bar data does not exit, please save data first"
+            return "bar data does not exit, please save data first."
         # 删除K线汇总数据
         if overview_symbol in self.library.list_symbols():
             self.library.delete(overview_symbol)
